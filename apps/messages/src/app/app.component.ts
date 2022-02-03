93a4d03aa60153a944ada5b43a60e9d23713ad8c
DXF0530 <dmitry.fleish@sbdinc.com>
2022-02-03 18:04:38 +0200

    Adding to main missing files

--- apps/messages/src/app/app.component.ts
+++ apps/messages/src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MessagesApiService} from './messages-api.service';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'stanley-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'messages';
+export class AppComponent implements OnInit {
+
+  message$: Observable<any>;
+  message = 'messages from app';
+
+  constructor(public messagesApiService: MessagesApiService) {
+  }
+
+  ngOnInit(): void {
+    this.message$ = this.messagesApiService.getMessage();
+  }
 }
--- apps/messages/src/app/app.module.ts
+++ apps/messages/src/app/app.module.ts
@@ -1,13 +1,16 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import {AppComponent} from './app.component';
+import {NxWelcomeComponent} from './nx-welcome.component';
+import {InfraModule} from '@stanley/infra';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, InfraModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+}
